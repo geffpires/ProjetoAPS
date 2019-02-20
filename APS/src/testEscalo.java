38adc767346706542491d6cd601eb1c4cdc0ac4c
LucaasM <lucas.calixto@dce.ufpb.br>
2019-02-20 15:54:16 -0300

    Criado teste de add processo e quantum

--- APS/src/testEscalo.java
+++ APS/src/testEscalo.java
@@ -8,15 +8,26 @@ import org.junit.jupiter.api.Test;
 class testEscalo {
 
 	@Test
-	public void testEscalonador() {
+	public void funcionaEscalonador() {
 		Escala escalonador = new Escala();
 		Assert.assertTrue("Escalonador está funcionando", escalonador.positivo());
-		
-		
-		Escala resultado = new Escala();
-		Assert.assertTrue("Processos estão escalonados", resultado.escalonamento());
+	}
+	@Test
+	public void testEscalonamento() {	
+		Escala escalonador = new Escala();
+		Assert.assertTrue("Processos estão escalonados", escalonador.escalonamento());
 		
 	}
+	@Test
+	public void testAdicionaProcessos() {
+		Escala escalonador = new Escala();
+		escalonador.addProcesso("P01", 0, 3);
 	
+	}
 	
+	@Test
+	public void testQuantum() {
+		Escala escalonador = new Escala();
+		escalonador.setQuantum(3);
+	}
 } 
