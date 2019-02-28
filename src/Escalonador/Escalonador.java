12a792e929af1ed151e725d720510cbc5e52df55
Gefferson Pires <gefferson.pires@dce.ufpb.br>
2019-02-28 15:25:05 -0300

    classe processo

--- src/Escalonador/Escalonador.java
+++ src/Escalonador/Escalonador.java
@@ -95,6 +95,10 @@ public class Escalonador {
 		}
 	}
 	
+	public void mudarDeLista(List<Process> p, List<Process> i) {
+		
+	}
+	
 	public void run() {
 
 		while (time < 0) {
---
+++ src/Process.java
@@ -0,0 +1,56 @@
+
+
+public class Process {
+	
+	private String name;
+	private int start;
+	private int runtime;
+	private char status;
+	private int onRun;
+	private String feedback = "";
+	
+	public Process(String name, int start, int runtime, int onRun) {
+		super();
+		this.name = name;
+		this.start = start;
+		this.runtime = runtime;
+		this.onRun = onRun;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getStart() {
+		return start;
+	}
+
+	public int getRuntime() {
+		return runtime;
+	}
+	
+	public void setRunTime() {
+		this.runtime--;
+		if (this.runtime == 0) {
+			this.setStatus('F');
+		}
+		this.addFeedback(this.getStatus());
+	}
+
+	public String getFeedback() {
+		return feedback;
+	}
+	
+	public char getStatus() {
+		return status;
+	}
+
+	public void setStatus(char status) {
+		this.status = status;
+	}
+
+	public void addFeedback(char statusRun) {
+		this.feedback += statusRun;
+	}
+
+}
