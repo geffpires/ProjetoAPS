a129de8d984dc7073fbd917f4b323a73ab9212f9
LucaasM <lucas.calixto@dce.ufpb.br>
2019-03-01 15:05:57 -0300

    metodo de teste

--- src/Escalonador/testEscalonador.java
+++ src/Escalonador/testEscalonador.java
@@ -4,14 +4,40 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import junit.framework.Assert;
+
+
+
+
+
 public class testEscalonador {
 	Escalonador escalonador = new Escalonador(3);
+	
 	@Test
-	public void addProcesso() {
+	public void testAddProcesso() {
 		
 		escalonador.addProcess("P01", 0, 3);
-		escalonador.addProcess("P02", 3, 6);
-		escalonador.addProcess("P03", 6, 9);
+		escalonador.addProcess("P02", 3, 3);
+		escalonador.addProcess("P03", 4, 6);
 	}	
 	
+	
+	// P01 0 3
+	// P02 3 3
+	// P03 4 6
+	@SuppressWarnings("deprecation")
+	@Test 
+	public void testRun() {
+		
+		String resultado = ""
+				+"p1: RRRWWWWWWRRF\n"
+				+"p2: IIIRRRFFFFFF\n"
+				+"p3: IIIIWWRRRFFF";
+		
+		
+		Assert.assertEquals(escalonador.graficoProcess(), resultado);
+		
+	
 }
+	}
+	
