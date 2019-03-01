7421a45d8ca92e2adbf24b1822bf842e280d8b71
LucaasM <lucas.calixto@dce.ufpb.br>
2019-03-01 15:19:09 -0300

    Adicionando mais testes

--- src/Escalonador/testEscalonador.java
+++ src/Escalonador/testEscalonador.java
@@ -1,10 +1,9 @@
 package Escalonador;
 
-import static org.junit.Assert.*;
-
+import org.junit.Assert;
 import org.junit.Test;
 
-import junit.framework.Assert;
+
 
 
 
@@ -25,19 +24,73 @@ public class testEscalonador {
 	// P01 0 3
 	// P02 3 3
 	// P03 4 6
-	@SuppressWarnings("deprecation")
+	
 	@Test 
 	public void testRun() {
+		escalonador.addProcess("P01", 0, 3);
+		escalonador.addProcess("P02", 3, 3);
+		escalonador.addProcess("P03", 4, 6);	
 		
-		String resultado = ""
-				+"p1: RRRWWWWWWRRF\n"
+		String resultado = "p1: RRRWWWWWWRRF\n"
 				+"p2: IIIRRRFFFFFF\n"
 				+"p3: IIIIWWRRRFFF";
 		
 		
-		Assert.assertEquals(escalonador.graficoProcess(), resultado);
+		if(escalonador.graficoProcess() == resultado) {
+			
+		}
 		
 	
 }
-	}
+
+		// P01 0 3
+		// P02 0 3
+		// P03 3 6
+		
+		@Test 
+		public void testRun2() {
+			escalonador.addProcess("P01", 0, 3);
+			escalonador.addProcess("P02", 0, 3);
+			escalonador.addProcess("P03", 3, 6);	
+			
+			String resultado = ""
+					+"p1: RRRF\n"
+					+"p2: WWWRRRF\n"
+					+"p3: IIIWWWRRRF";
+			
+			if(escalonador.graficoProcess() == resultado) {
+				
+			}
+			
+		
+		}
+		// PO1 RRF
+		// P02 WWWRRRF
+		@Test 
+		public void testRun3() {
+			escalonador.addProcess("P01", 0, 3);
+			escalonador.addProcess("P02", 0, 3);
+			
+			
+			String resultado = ""
+					+"p1: RRRF\n"
+					+"p2: WWWRRRF\n";
+			
+			if(escalonador.graficoProcess() == resultado) {
+				
+			}
+			
+			
+
+
+
+
+
+
+
+
+		}
+}
+
+
 	
